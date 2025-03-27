0c917515ccf44855299d3d99b602be10e723b92a
David <david.samah@student.moringaschool.com>
2025-03-27 10:05:45 +0300

    submission

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+function writeCards(names, event){
+
+
+    const thankyouMessage = [];
+
+    for(let i = 0;i<names.length; ++i){
+
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+
+        thankyouMessage.push(message)
+    }
+
+    return thankyouMessage
+}
+
+function countDown(start){
+
+
+    while (start >-1 ){
+
+        console.log(start);
+
+        start --
+    }
+}
+
+countDown(10)
